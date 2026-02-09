814cc01ed3577a68deaba8981d1355113c259f0d
aishtiaq <awshafishtiaque@gmail.com>
2026-02-09 11:08:31 -0800

    update

--- __tests__/easy.test.js
+++ __tests__/easy.test.js
@@ -1,5 +1,22 @@
-import { add } from '../functions/easy.js';
+import { add } from "../functions/easy.js";
 
-test('adds 1 + 2 to equal 3', () => {
+describe("add function", () => {
+  test("adds 1 + 2 to equal 3", () => {
     expect(add(1, 2)).toBe(3);
+  });
+
+  test("add 5 + 10 to equal 15", () => {
+    expect(add(5, 10)).toBe(15);
+  });
+
+  test("add 0 + 0 to equal 0", () => {
+    expect(add(0, 0)).toBe(0);
+  });
+});
+
+describe("add function with negative numbers", () => {
+    test("adds -1 + -2 to equal -3", () => {
+      expect(add(-1, -2)).toBe(-3);
+    });
+
 });
