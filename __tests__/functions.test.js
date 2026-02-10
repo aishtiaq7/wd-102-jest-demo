25cb7150a8fa1081c5a36ec8af16b178c1f7743f
aishtiaq <awshafishtiaque@gmail.com>
2026-02-10 11:47:08 -0800

    update

--- __tests__/functions.test.js
+++ __tests__/functions.test.js
@@ -1,16 +1,56 @@
-import { shout  } from "../functions/functions";
+import { shout, sumAndNotify, repeatMessage } from "../functions/functions";
 import { jest } from "@jest/globals";
 
-describe("Moc functions", ()=> {
-    test("shout uppercase and logs", ()=>{
+describe("Moc functions", () => {
+  test("shout uppercase and logs", () => {
+    const logger = jest.fn();
 
-        const logger = jest.fn();
-    
-        const result = shout("hello", logger);
+    const result = shout("hello", logger);
 
-        expect (result).toBe("HELLO");
-        expect (logger).toHaveBeenCalledTimes(1);
-        expect(logger).toHaveBeenCalledWith("HELLO");
+    expect(result).toBe("HELLO");
+    expect(logger).toHaveBeenCalledTimes(1);
+    expect(logger).toHaveBeenCalledWith("HELLO");
 
-    })
+  expect(() => {
+      shout("random", "not a function");
+    }).toThrow("logFun must be a function");
+
+  });
+});
+
+describe("sumAndNotify", () => {
+  test("shout uppercase and logs", () => {
+    const notify = jest.fn();
+
+    const result = sumAndNotify(3, 4, notify);
+    expect(result).toBe(7);
+    expect(notify).toHaveBeenCalledWith(7);
+
+    expect(() => {
+      sumAndNotify(3, 4, "not a function");
+    }).toThrow("notifyFn must be a function");
+  });
+});
+
+
+describe("repeatMessage", ()=>{
+    test("errors", ()=>{
+        expect(()=> {
+            repeatMessage("hello", 8, "BAD I    NPUT")
+        }).toThrow("logFun must be a function");
+    });
+
+     test("errors for bad 2nd param", ()=>{
+        expect(()=> {
+            repeatMessage("hello", 4.1, ()=>{})
+        }).toThrow("times must be a non-negative integer");
+    }); 
+
+    test("valid params of repeatMessages", ()=>{  
+        const notify = jest.fn();
+
+        expect(repeatMessage("hi", 3, notify )).toBe(3)
+        expect(notify).toHaveBeenCalledTimes(3);
+
+    });
 })
--- functions/functions.js
+++ functions/functions.js
@@ -1,10 +1,34 @@
-export function shout ( message, logFun ) {
-    if ( typeof logFun !== 'function' ) {
-        throw new Error( 'logFun must be a function' );
-    }
-    const result = String(message).toUpperCase();
-    logFun(result); // console.log("HELLO");
-    return result; 
+export function shout(message, logFun) {
+  if (typeof logFun !== "function") {
+    throw new Error("logFun must be a function");
+  }
+  const result = String(message).toUpperCase();
+  logFun(result); // console.log("HELLO");
+  return result;
 }
 
-// shout("hello", console.log);
+export function sumAndNotify(a, b, notifyFn) {
+  if (typeof notifyFn !== "function") {
+    throw new Error("notifyFn must be a function");
+  }
+  const sum = a + b;
+  notifyFn(sum);
+  return sum;
+}
+
+// console.log( typeof "sdfijwoefhwoiehf");
+
+export function repeatMessage(message, times, logFun) {
+  if (typeof logFun !== "function") {
+    throw new Error("logFun must be a function");
+  }
+
+  if (!Number.isInteger(times) || times < 0) {
+    throw new Error("times must be a non-negative integer");
+  }
+
+  for (let i = 0; i < times; i++) {
+    logFun(message);
+  }
+  return times;
+}
